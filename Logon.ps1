61f3225ebb82ee62a40ec7388bb7f347a025a652
rickstok <r.stokkingreef@compello.nl>
2014-01-14 11:01:07 +0100

    Update Logon.ps1
    Failed on execution in W2k8-r2

--- Logon.ps1
+++ Logon.ps1
@@ -2,19 +2,19 @@ $ErrorActionPreference = "Stop"
 
 try
 {
-    $Host.UI.RawUI.WindowTitle = "Downloading PSWindowsUpdate..."
+#    $Host.UI.RawUI.WindowTitle = "Downloading PSWindowsUpdate..."
 
-    $psWindowsUpdateUrl = "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/25/PSWindowsUpdate.zip"
-    $psWindowsUpdatePath = "$ENV:Temp\PSWindowsUpdate.zip"
-    (new-object System.Net.WebClient).DownloadFile($psWindowsUpdateUrl, $psWindowsUpdatePath)
+#    $psWindowsUpdateUrl = "http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/25/PSWindowsUpdate.zip"
+#    $psWindowsUpdatePath = "$ENV:Temp\PSWindowsUpdate.zip"
+#    (new-object System.Net.WebClient).DownloadFile($psWindowsUpdateUrl, $psWindowsUpdatePath)
 
-    $Host.UI.RawUI.WindowTitle = "Installing PSWindowsUpdate..."
-    foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdatePath).Items())
-    {
-        $yesToAll = 16
-        (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
-    }
-    del $psWindowsUpdatePath
+#    $Host.UI.RawUI.WindowTitle = "Installing PSWindowsUpdate..."
+#    foreach($item in (New-Object -com shell.application).NameSpace($psWindowsUpdatePath).Items())
+#    {
+#        $yesToAll = 16
+#        (New-Object -com shell.application).NameSpace("$ENV:SystemRoot\System32\WindowsPowerShell\v1.0\Modules").copyhere($item, $yesToAll)
+#    }
+#    del $psWindowsUpdatePath
 
     Import-Module PSWindowsUpdate
 
